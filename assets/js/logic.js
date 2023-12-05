63751b082b58abd9c0141834a1f886a6c2a0fd17
AvniB82 <bicmeti.avni@gmail.com>
2023-12-05 22:36:26 +0000

    added code to start and display qiesions

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,5 +1,8 @@
 var questions = [];
 
+
+document.addEventListener("DOMContainerLoaded", function() {
+
 var startButton = document.getElementById("start");
 var timeElement = document.getElementById("time");
 var submitButton = document.getElementById("submit");
@@ -7,4 +10,41 @@ var questionsContainer = document.getElementById("questions");
 var feedbackContainer = document.getElementById("feedback");
 var initialsUnput = document.getElementById("initials");
 
-document.addEventListener("DOMContainerLoaded", function(){}
+let currentQuestionIndex = 0;
+let timer;
+
+startButton.addEventListener("click", startQuiz);
+submitButton.addEventListener("click", submitScore);
+
+// start quiz
+function startQuiz() {
+    startButton.style.display = "none";
+    questionsContainer.classList.remove("hide");
+
+    displayQuestion(currentQuestionIndex);
+    startTimer();
+
+    submitButton.styleDisplay = "none";
+}
+
+// displaying questions
+function displayQuesation(index) {
+    var questionsTitle = document.getAnimations("questions-title");
+    var choiceContainer = doccument.getElementById("choice");
+
+
+    questionsTitle.textContent = questions[index].questions;
+    choiceContainer.innnerHTML = "";
+
+    for (let i = 0; i < questions[index].choices.length; i++) {
+        var choiceButton = document.createElement("button");
+        choiceButton.textContent = questions[index].choioces[i];
+        choiceButton.addEventListener("click", function () {
+            checkAnswer(choiceButton.textContent);
+        });
+    }
+}
+
+
+
+}
