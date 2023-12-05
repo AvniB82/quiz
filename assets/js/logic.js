a9cab5edf2f7b2e78a3522d74eae02953dd33bb2
AvniB82 <bicmeti.avni@gmail.com>
2023-12-05 22:19:26 +0000

    added DOM elements

---
+++ assets/js/logic.js
@@ -0,0 +1,10 @@
+var questions = [];
+
+var startButton = document.getElementById("start");
+var timeElement = document.getElementById("time");
+var submitButton = document.getElementById("submit");
+var questionsContainer = document.getElementById("questions");
+var feedbackContainer = document.getElementById("feedback");
+var initialsUnput = document.getElementById("initials");
+
+document.addEventListener("DOMContainerLoaded", function(){}
---
+++ assets/js/questions.js
@@ -0,0 +1,23 @@
+const questions = [
+
+{
+  question: "question 1",
+  wrongAnswers: ["b", "c", "d"],
+  correctAnswer: "a"
+},
+{
+  question: "question 2",
+  wrongAnswers: ["a", "c", "d"],
+  correctAnswer: "b"
+},
+{
+    question: "questions 3 ",
+    wrongAnswers: ["a", "b", "d"],
+    correctAnswer: "c"
+  },
+  {
+    question: "questions 4 ",
+    wrongAnswers: ["a", "b", "c"],
+    correctAnswer: "d"
+  }
+];
