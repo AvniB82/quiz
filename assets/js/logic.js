da6bb66ee12eaf4b05e534c8abe559d45db6233c
AvniB82 <bicmeti.avni@gmail.com>
2023-12-06 18:32:32 +0000

    added submit score

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -10,8 +10,8 @@ document.addEventListener("DOMContentLoaded", function() {
     var currentQuestionIndex = 0;
     var timer;
 
-startButton.addEventListener("click", startQuiz);
-submitButton.addEventListener("click", submitScore);
+    startButton.addEventListener("click", startQuiz);
+    submitButton.addEventListener("click", submitScore);
 
 // start quiz
 function startQuiz() {
@@ -29,91 +29,93 @@ function displayQuestion(index) {
     var questionTitle = document.getElementById("question-title");
     var choicesContainer = document.getElementById("choices");
 
-
     questionTitle.textContent = questions[index].question;
-    questionsContainer.innerHTML = "";
+    choicesContainer.innerHTML = "";
 
     var allChoices = [...questions[index].wrongAnswers, questions[index].correctAnswer];
     shuffleArray(allChoices);
-
 
     for (var i = 0; i < allChoices.length; i++) {
         var choiceButton = document.createElement("button");
         choiceButton.textContent = allChoices[i];
         choiceButton.addEventListener("click", function () {
-          checkAnswer(choiceButton.textContent);
+            checkAnswer(choiceButton.textContent);
         });
         choicesContainer.appendChild(choiceButton);
-      }
     }
+}
+
 function shuffleArray(array) {
     for (var i = array.length - 1; i > 0; i--) {
         var j = Math.floor(Math.random() * (i + 1));
         [array[i], array[j]] = [array[j], array[i]];
     }
-
 }
 
-// displaying correct or incorrect message
-function checkAnswer(selectedAnswer) {
-    if (selectedAnswer === questions[currentQuestionIndex].correctAnswer) {
-        showFeedback ("correct");
-    }
-    else {
-        showFeedback("incorrect");
+    function checkAnswer(selectedAnswer) {
+        if (selectedAnswer === questions[currentQuestionIndex].correctAnswer) {
+            showFeedback("Correct!");
+        } else {
+            showFeedback("Incorrect!");
+            deductTime();
+        }
+
+        currentQuestionIndex++;
+
+        if (currentQuestionIndex < questions.length) {
+            displayQuestion(currentQuestionIndex);
+        } else {
+            endQuiz();
+        }
     }
 
-    currentQuestionIndex++;
-    }
-
-    if (currentQuestionIndex < questions.length) {
-        displayQuestion(currentQuestionIndex);
-    }
-    else {
-        endQuiz();
-    }
-
-function showFeedback(message) {
-    feedbackContainer.textContent = message;
-    feedbackContainer.classList.remove("hide");
-    setTimeout(() => {
-        feedbackContainer.classList.add("hide");
+    function showFeedback(message) {
+        feedbackContainer.textContent = message;
+        feedbackContainer.classList.remove("hide");
+        setTimeout(() => {
+            feedbackContainer.classList.add("hide");
         }, 1000);
     }
 
+    function deductTime() {
+        var currentTime = parseInt(timeElement.textContent);
+        if (currentTime >= 10) {
+            updateTime(currentTime - 10);
+        } else {
+            updateTime(0);
+        }
+    }
 
-function startTimer() {
-    let timeLeft = 60;
-    updateTime(timeLeft);
-    
-    timer = setInterval(function () {
-        timeLeft--;
+    function startTimer() {
+        var timeLeft = 60;
         updateTime(timeLeft);
-    
-        if (timeLeft <= 0) {
-            endQuiz();
+
+        timer = setInterval(function () {
+            timeLeft--;
+            updateTime(timeLeft);
+
+            if (timeLeft <= 0) {
+                endQuiz();
             }
         }, 1000);
     }
 
+    function updateTime(time) {
+        timeElement.textContent = time;
+    }
 
-function updateTime(time) {
-    timeElement.textContent = time;
-        }
-    
     function endQuiz() {
         clearInterval(timer);
         questionsContainer.classList.add("hide");
         document.getElementById("end-screen").classList.remove("hide");
         document.getElementById("final-score").textContent = timeElement.textContent;
         submitButton.style.display = "block";
-      }
-    
+    }
+
     function submitScore() {
         var initials = initialsInput.value.trim();
-        if (initials !=="") {
+        if (initials !== "") {
             console.log("Score submitted:", initials, timeElement.textContent);
         }
     }
-
-    });
+});
